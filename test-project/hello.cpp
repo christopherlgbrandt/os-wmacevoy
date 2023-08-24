9f56a71610fc701d3d50ff9670467e0f824c8e56
Christopher Brandt <christopherlgbrandt@gmail.com>
2023-08-24 14:12:17 -0600

    Create hello.cpp

---
+++ test-project/hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int x = 3456;
+
+void say_hello()
+{
+    string message = "hello";
+    cout << message << endl;
+}
+
+int main() 
+{
+    say_hello();
+    return 0;
+}
